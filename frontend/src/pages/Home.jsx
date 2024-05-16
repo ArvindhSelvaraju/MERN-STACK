40757a2c25639a6d423000df9d1e9b539de1cc29
ArvindhSelvaraju <arvindhnkl2004@gmail.com>
2024-05-16 13:31:15 +0530

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+const Home = () => {
+  // const [workouts, setWorkouts] = useState(null);
+  const {workouts, dispatch} = useWorkoutsContext();
+
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        // setWorkouts(json);
+        dispatch({type:'SET_WORKOUTS', payload:json})
+      }
+    };
+
+    fetchWorkout();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
+
+export default Home;
